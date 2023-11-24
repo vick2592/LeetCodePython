7bc2580820b1335da7ac6fe8173e97a95c7f8c00
vick2592 <vick2592@yahoo.com>
2023-11-23 23:56:01 -0500

    815 Solution updated

--- LeetCodeProblems/815BusRoutes.py
+++ LeetCodeProblems/815BusRoutes.py
@@ -17,6 +17,8 @@ class Solution:
         while len(queueRoutes) > 0:
             testRoute = queueRoutes[0]
             queueRoutes.pop(0)
+            routes2pop = []
+            popCount = 0
             for i in range(len(routes)):
                 if testRoute in routes[i]:
                     for j in routes[i]:
@@ -25,8 +27,10 @@ class Solution:
                         if j not in list(queueDict.keys()):
                             queueDict[j] = queueDict[testRoute] + 1
                             queueRoutes.append(j)
-                    routes.pop(i)
-                    break
+                    routes2pop.append(i)
+            for k in routes2pop:
+                routes.pop(k - popCount)
+                popCount += 1
             
     
         if(target not in list(queueDict.keys())):
@@ -38,9 +42,9 @@ routes = [[1,2,7],[3,6,7]]
 source = 1
 target = 6
 
-routes = [[7,12],[4,5,15],[6],[15,19],[9,12,13]]
-source = 15
-target = 12
+# routes = [[7,12],[4,5,15],[6],[15,19],[9,12,13]]
+# source = 15
+# target = 12
 
 answer = Solution.numBusesToDestination(routes, source, target)
 print(answer)
