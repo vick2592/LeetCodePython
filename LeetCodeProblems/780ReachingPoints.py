08cf29746cec1456df124437186c73dc58ebeae4
vick2592 <vick2592@yahoo.com>
2023-11-28 02:00:39 -0500

    Started 780

---
+++ LeetCodeProblems/780ReachingPoints.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reachingPoints(sx, sy, tx, ty):
+        return False
+    
+sx = 1
+sy = 1
+tx = 3
+ty = 5
+
+answer = Solution.reachingPoints(sx, sy, tx, ty)
+print(answer)
