d98e7aebabe7cc4d83ee6f3945ceb8f0e3e089fa
vick2592 <vick2592@yahoo.com>
2023-11-28 02:38:27 -0500

    Updated 780 part 2

--- LeetCodeProblems/780ReachingPoints.py
+++ LeetCodeProblems/780ReachingPoints.py
@@ -1,11 +1,27 @@
 class Solution:
-    def reachingPoints(sx, sy, tx, ty):
-        return False
+    def reachingPoints(self, sx, sy, tx, ty, ans = False, count = 0):
+        if ans == True:
+            return ans
+        if count > 10:
+            return ans
+        if sx == tx and sy == ty:
+            ans = True
+            print("True")
+            return ans
+        
+        self.reachingPoints(sx, sx+sy, tx, ty, ans, count = count + 1)
+        self.reachingPoints(sx+sy, sy, tx, ty, ans, count = count + 1)
     
 sx = 1
 sy = 1
 tx = 3
 ty = 5
 
-answer = Solution.reachingPoints(sx, sy, tx, ty)
-print(answer)
+sx = 1
+sy = 1
+tx = 2
+ty = 2
+
+answer = Solution()
+print("The answer is: ", answer.reachingPoints(sx, sy, tx, ty))
+
