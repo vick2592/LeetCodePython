37203264196fe297c38023788bbb5a187a367189
vick2592 <vick2592@yahoo.com>
2024-02-12 19:43:44 -0500

    1860 Solution added

--- LeetCodeProblems/1860IncrementalMemoryLeak.py
+++ LeetCodeProblems/1860IncrementalMemoryLeak.py
@@ -1,1 +1,17 @@
+class Solution:
+    def memLeak(memory1, memory2):
+        x = 1
+        while (memory1 >= x or memory2 >= x):
+            if memory1 >= memory2:
+                memory1 -= x
+            else:
+                memory2 -= x
+            x += 1
+        ans = [x, memory1, memory2]    
+        return ans
+    
+memory1 = 8
+memory2 = 11
 
+ans = Solution.memLeak(memory1, memory2)
+print(ans)
