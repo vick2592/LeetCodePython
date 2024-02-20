fa3d1dce650b8ca182b02d1f85db61f928a2aba1
vick2592 <vick2592@yahoo.com>
2024-02-20 14:28:30 -0500

    Working on 2030.

---
+++ LeetCodeProblems/2030SmallestSubstrWithOccurrenceLetter.py
@@ -0,0 +1,11 @@
+class Solution:
+    def smallestSubsequence(s, k, letter, repetition):
+        return "hello world"
+    
+s = "leetcode"
+k = 4
+letter = "e"
+repetition = 2
+
+ans = Solution.smallestSubsequence(s, k, letter, repetition)
+print(ans)
