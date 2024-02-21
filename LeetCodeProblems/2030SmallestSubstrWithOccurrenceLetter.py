40cf1eb0a2c8ce45ee3ebd68eba9ea513ac85db9
vick2592 <vick2592@yahoo.com>
2024-02-20 20:36:20 -0500

    2030 Solution added

--- LeetCodeProblems/2030SmallestSubstrWithOccurrenceLetter.py
+++ LeetCodeProblems/2030SmallestSubstrWithOccurrenceLetter.py
@@ -1,6 +1,29 @@
 class Solution:
     def smallestSubsequence(s, k, letter, repetition):
-        return "hello world"
+        dp = []
+        def dps(ind, k, strng, word = ""):
+            if len(word) == k:
+                dp.append(word)
+                return
+            if ind >= len(strng):
+                return
+            dps(ind + 1, k, strng, word)
+            dps(ind + 1, k, strng, word + strng[ind])
+            
+        dps(0, k, s)
+        # print(dp)
+        ans = []
+        for w in dp:
+            count = 0
+            for i in w:
+                if i == letter:
+                    count += 1
+                if count == repetition:
+                    ans.append(w)
+                    break
+        print(ans)
+        ans = sorted(ans)
+        return ans[0]
     
 s = "leetcode"
 k = 4
