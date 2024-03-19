af14612b0c6b20126f4302a1a68b28a14d23491c
vick2592 <vick2592@yahoo.com>
2024-03-19 01:32:13 -0400

    Solution 1171 added

---
+++ LeetCodeProblems/1171RemoveZeroSumNodesInList.py
@@ -0,0 +1,55 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeZeroSumSublists(head):
+        
+        indexTrim = []
+        for b in range(len(head)):
+            count = 0
+            for c in range(b, len(head)):
+                count += head[c]
+                if count == 0:
+                    indexTrim += [[i for i in range(b, c+1)]]
+                    count = 0
+        print(indexTrim)
+        finished = False
+        for k in indexTrim:
+            j = 0
+            for i in k:
+                head.pop(i-j)
+                j+= 1
+            stillZero = False
+            for julie in range(len(head)):
+                testCount = 0
+                for vaiman in range(julie, len(head)):
+                    testCount = testCount + head[vaiman]
+                    if testCount == 0:
+                        stillZero = True
+                        break
+                if stillZero:
+                    break
+            if stillZero == False:
+                break
+        # ZeroSum = False
+        # test = head[0]
+        # x = 0
+        # while x != len(head)-1:
+        #     test += head[x + 1]
+        #     print(test)
+        #     if test == 0:
+        #         ZeroSum = True
+        #         head.pop(x + 1)
+        #         x = 0
+        #         test = head[0]
+        #     x += 1
+        
+        return head
+    
+head = [1,2,-3,3,1]
+head = [1,2,3,-3,4]
+ans = Solution.removeZeroSumSublists(head)
+print(ans)  # Output: [3,1]
+        
