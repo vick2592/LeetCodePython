053731899c5ce12c8a9f9deb0339ac53f8239864
vick2592 <vick2592@yahoo.com>
2024-09-24 20:29:17 -0400

    1703 Updated

--- LeetCodeProblems/1703MinAdjacentSwapsKOnes.py
+++ LeetCodeProblems/1703MinAdjacentSwapsKOnes.py
@@ -1,19 +1,5 @@
 class Solution:
     def minMoves(nums, k):
-        def check(arr, kTest):
-            count = 0
-            for i in arr:
-                if i == 1:
-                    count += 1
-                if count == kTest:
-                    return True
-                if i != 1:
-                    count = 0
-            return False
-
-        if check(nums, k):
-            return 0
-
         distances = []
         count = 0
         for i in range(len(nums)):
@@ -37,8 +23,8 @@ k = 3
 [1,1,0,1]
 k = 2
 
-nums = [1,0,0,1,0,1]
-k = 2
+# nums = [1,0,0,1,0,1]
+# k = 2
 answer = Solution.minMoves(nums, k)
 
 print(answer)  # Expected output: 3
