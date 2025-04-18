d6feb3e5f6ffdb9e648d1a569a60f5c99a36b5bb
vick2592 <vick2592@yahoo.com>
2025-04-17 23:22:42 -0400

    658 solution created

---
+++ LeetCodeProblems/658KClosestElements.py
@@ -0,0 +1,41 @@
+class Solution:
+    def findClosestElements(arr, k, x):
+        answer = []
+        xPos = -1
+        for i in range(len(arr)):
+            if arr[i] == x:
+                xPos = i
+                break
+        if xPos == -1:
+            if arr[0] > x:
+                return sorted(arr[:k])
+            elif arr[-1] < x:
+                return sorted(arr[-k:])
+        else:
+            if xPos <= k:
+                return sorted(arr[:k])
+            elif xPos > k:
+                lpos = xPos - k
+                rpos = lpos + k
+                return sorted(arr[lpos:rpos])
+
+            return []
+
+arr = [1,2,3,4,5]
+k = 4
+x = 3
+
+# arr = [1,1,2,3,4,5]
+# k = 4
+# x = -1
+
+arr = [1, 2, 3, 10, 20]
+k = 3
+x = 3
+
+arr = [1, 2, 3, 4, 10]
+k = 3
+x = 4
+
+answer = Solution.findClosestElements(arr, k, x)
+print(answer)  # Expected output: [1,2,3,4]
