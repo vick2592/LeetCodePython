1d2740367718cbe0a3a18084c31ac6eb68aada83
vick2592 <vick2592@yahoo.com>
2025-04-18 15:59:44 -0400

    Solution for 1847 added

--- LeetCodeProblems/1847ClosestRoom.py
+++ LeetCodeProblems/1847ClosestRoom.py
@@ -1,12 +1,28 @@
 class Solution:
     def closestRoom(rooms, queries):
         answer = []
+        for x in range(len(queries)):
+            minRoom = [-1, -1]
+            for y in range(len(rooms)):
+                if queries[x][1] > rooms[y][1]:
+                    continue
+                else:
+                    absDiff = abs(queries[x][0] - rooms[y][0])
+                    if absDiff < minRoom[1] or minRoom[1] == -1:
+                        minRoom = [rooms[y][0], absDiff]
+                    elif absDiff == minRoom[1]:
+                        if rooms[y][0] < minRoom[0]:
+                            minRoom = [rooms[y][0], absDiff]
+
+            answer.append(minRoom[0])
 
         return answer
 
 rooms = [[2,2],[1,2],[3,2]]
 queries = [[3,1],[3,3],[5,2]]
-        
+
+rooms = [[1,4],[2,3],[3,5],[4,1],[5,2]]
+queries = [[2,3],[2,4],[2,5]]
 
 answer = Solution.closestRoom(rooms, queries)
 print(answer)  # Expected output: [1,2,3]
