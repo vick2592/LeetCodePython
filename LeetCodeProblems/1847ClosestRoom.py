c3decee9d220a1e98c0528cb4e9f572202837795
vick2592 <vick2592@yahoo.com>
2025-04-18 00:27:15 -0400

    Started 1847

---
+++ LeetCodeProblems/1847ClosestRoom.py
@@ -0,0 +1,12 @@
+class Solution:
+    def closestRoom(rooms, queries):
+        answer = []
+
+        return answer
+
+rooms = [[2,2],[1,2],[3,2]]
+queries = [[3,1],[3,3],[5,2]]
+        
+
+answer = Solution.closestRoom(rooms, queries)
+print(answer)  # Expected output: [1,2,3]
