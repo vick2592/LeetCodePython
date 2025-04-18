f6a92dad0f4d760f073ee1db1a9d11377d649414
vick2592 <vick2592@yahoo.com>
2025-04-17 23:30:51 -0400

    658 updated

--- LeetCodeProblems/658KClosestElements.py
+++ LeetCodeProblems/658KClosestElements.py
@@ -4,7 +4,7 @@ class Solution:
         xPos = -1
         for i in range(len(arr)):
             if arr[i] == x:
-                xPos = i
+                xPos = i+1
                 break
         if xPos == -1:
             if arr[0] > x:
@@ -19,23 +19,21 @@ class Solution:
                 rpos = lpos + k
                 return sorted(arr[lpos:rpos])
 
-            return []
-
 arr = [1,2,3,4,5]
 k = 4
 x = 3
 
-# arr = [1,1,2,3,4,5]
-# k = 4
-# x = -1
+arr = [1,1,2,3,4,5]
+k = 4
+x = -1
 
-arr = [1, 2, 3, 10, 20]
-k = 3
-x = 3
+# arr = [1, 2, 3, 10, 20]
+# k = 3
+# x = 3
 
-arr = [1, 2, 3, 4, 10]
-k = 3
-x = 4
+# arr = [1, 2, 3, 4, 10]
+# k = 3
+# x = 4
 
 answer = Solution.findClosestElements(arr, k, x)
 print(answer)  # Expected output: [1,2,3,4]
